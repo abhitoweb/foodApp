1075f5ac9aa3f466925cad1368345912b546db8c
Abhishek Pandey <Abhishek Pandey>
2017-06-06 03:21:30 +0530

    updated foodApp

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -11,9 +11,13 @@ export class RecipeListComponent implements OnInit {
     new Recipe('Somosa','A delicious north Indian Somasa','https://i.ytimg.com/vi/iIVJN0Yz1Y0/maxresdefault.jpg'),
     new Recipe('Chole Bature','A mouth watering original street taste','https://i0.wp.com/files.hungryforever.com/wp-content/uploads/2016/10/18160841/best-chole-bhature-in-mumbai.jpg')
   ];
+  @Output()recipeWasSelected = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit() {
+  }
+  onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
